60868c78b3161a3172f0537f63c54992a93ea08b
Subha <fabihafairuzz@gmail.com>
2022-03-02 10:34:16 -0700

    Project.java and ProjectTest.java file modified

--- Main Project/src/Project.java
+++ Main Project/src/Project.java
@@ -2,8 +2,22 @@ import java.util.Scanner;
 import java.util.ArrayList;
 import java.util.HashMap;
 
+/**
+ *         A fitness tracking program that allows the user to input their information and get back various results form that
+ *         *Members:
+ *         Fabiha Fairuzz Subha (UCID: 30148674)
+ *         Amasil Rahim Zihad (UCID: 30164830)
+ *         * Date: 1 March, 2022
+ *         * Demo 1 Tutorial 10
+ */
 
 public class Project {
+    /**
+     * Start the tracking program
+     *
+     * @param args No arguments expected
+     */
+
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.println("Welcome to this fitness tracking program");
--- Main Project/src/ProjectTest.java
+++ Main Project/src/ProjectTest.java
@@ -1,3 +1,14 @@
+/**
+ * A fitness tracking program that allows the user to input their information and get back various results form that
+ * <p>
+ * *Members:
+ * Fabiha Fairuzz Subha (UCID: 30148674)
+ * Amasil Rahim Zihad (UCID: 30164830)
+ * * Date: 1 March 2022
+ * * Demo 1 Tutorial 10
+ */
+
+
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
