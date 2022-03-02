db82e48752d130a958662a38e1dae31bdc7e1733
Subha <fabihafairuzz@gmail.com>
2022-03-01 20:42:28 -0700

    Commenting and debugged the Project.java file

--- Main Project/src/Project.java
+++ Main Project/src/Project.java
@@ -1,4 +1,3 @@
-import java.time.Year;
 import java.util.Scanner;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -18,6 +17,9 @@ public class Project {
 
     }
 
+    /**
+     * Function to get the user info
+     */
     public static void getUserInfo() {
         Scanner scan = new Scanner(System.in);
         System.out.println("Enter your name: ");
@@ -42,6 +44,7 @@ public class Project {
         userInfo.add(userAge);
         userInfo.add(userWeight);
         userInfo.add(userHeight);
+        //Including the array in a hashmap and assigning the key to be the name of the user
         HashMap<String, ArrayList> userInfo2 = new HashMap<>();
         userInfo2.put(userName, userInfo);
         outMenu(userName, userGender, userAge, userWeight, userHeight, userInfo2);
@@ -49,6 +52,16 @@ public class Project {
 
     }
 
+    /**
+     *
+     * @param userName The name of the user
+     * @param userGender The gender of the user
+     * @param userAge The age of the user
+     * @param userWeight The weight of the user
+     * @param userHeight The height of the user
+     * @param userInfo2 The hashmap where the user information are stored
+     * @return A variable to check the choice input of the user
+     */
     public static int outMenu(String userName, String userGender, double userAge, double userWeight, double userHeight, HashMap<String, ArrayList> userInfo2) {
         int checkinput;
         do {
@@ -65,6 +78,7 @@ public class Project {
             Scanner input = new Scanner(System.in);
             checkinput = input.nextInt();
             if (checkinput == 1) {
+                //Calling the print user info function to print all the information
                 printUserInfo(userName, userGender, userAge, userWeight, userHeight, userInfo2);
 
             } else if (checkinput == 2) {
@@ -72,27 +86,30 @@ public class Project {
                 System.out.println("Do you want to change your inputs: (Press Y to change or N to return to menu)");
                 String userinput;
                 userinput = input.nextLine();
+                //Calling the change input function to enable the user to re-enter the details
                 inputChange(userinput);
 
             } else if (checkinput == 3) {
+                //Calculating and printing out BMI using BMI function
                 double BMI = BMI(userWeight, userHeight);
                 System.out.println(String.format("Your BMI: %.2f", BMI));
 
             } else if (checkinput == 4) {
+                //Comparing and printing out the statement using BMI_compare function
                 double BMI = BMI(userWeight, userHeight);
                 System.out.println(BMI_Compare(BMI));
 
             } else if (checkinput == 5) {
-
+                //Calculating the total calories need to burn according the user input using estimateCalories function
                 System.out.println("You need to burn " + estimateCalories() + " calories");
 
             } else if (checkinput == 6) {
-
+                //Calculating the total number of hours needed to exercise using the estimateExercise function
                 int total_calories = estimateCalories();
                 estimateExercise(total_calories, userWeight);
 
             } else {
-
+                //Program should exit if the user input matches 0 or anything else that is not defined in the menu
                 System.out.println("Program Exited");
             }
 
@@ -102,10 +119,20 @@ public class Project {
 
     }
 
-
+    /**
+     *
+     * @param userName The name of the user
+     * @param userGender The gender of the user
+     * @param userAge The age of the user
+     * @param userWeight The weight of the user
+     * @param userHeight The height of the user
+     * @param userInfo2 The hashmap where the user information are stored
+     *
+     */
     public static void printUserInfo(String userName, String userGender, double userAge, double userWeight, double userHeight, HashMap<String, ArrayList> userInfo2) {
         userInfo2.get(1);
 
+        //Printing out the user information
         System.out.println("You are " + userName);
         System.out.println("Your weight is " + userWeight + " kg");
         System.out.println("Gender:" + userGender);
@@ -114,6 +141,10 @@ public class Project {
 
     }
 
+    /**
+     *
+     * @param checkinput A variable created to track the user input
+     */
     public static void inputChange(String checkinput) {
         Scanner input = new Scanner(System.in);
 
@@ -136,6 +167,8 @@ public class Project {
             userHeight = input.nextDouble();
             System.out.println("Do you want to change your inputs: (Press Y to change or N to return to menu)");
             checkinput = input.nextLine();
+
+            //Storing the newly entered data to update the Array and HashMap
             ArrayList<Double> userInfo = new ArrayList<Double>();
             userInfo.add(userAge);
             userInfo.add(userWeight);
@@ -148,6 +181,12 @@ public class Project {
 
     }
 
+    /**
+     *
+     * @param userWeight The weight of the user
+     * @param userHeight The height of the user
+     * @return The calculated BMI of the user
+     */
     public static double BMI(double userWeight, double userHeight) {
         //cm square to meter square conversion constant
         final int CONVERT = 10000;
@@ -156,6 +195,11 @@ public class Project {
         return BMI;
     }
 
+    /**
+     *
+     * @param BMI The calculated BMI of the user
+     * @return A string to give the user weight status
+     */
     public static String BMI_Compare(double BMI) {
 
         if (BMI < 18.5) {
@@ -172,6 +216,10 @@ public class Project {
             return "You are obese";
     }
 
+    /**
+     *
+     * @return Total number of calories needed to burn to lose the user inputted weight
+     */
     public static int estimateCalories() {
 
         //Calories needed to burn 1 kg
@@ -187,8 +235,14 @@ public class Project {
         return total_calories;
     }
 
+    /**
+     *
+     * @param total_calories Total number of calories needed to burn to lose the user inputted weight
+     * @param userWeight The weight of the user
+     */
     public static void estimateExercise(int total_calories, double userWeight) {
         Scanner input = new Scanner(System.in);
+        //Giving the user to chose the type of exercise they want to do
         System.out.print("""
                 Which type of exercise do you want to do?
                 A. Cycling
@@ -196,11 +250,12 @@ public class Project {
                 """);
         String userinput;
         userinput = input.nextLine();
-        if (userinput.equals("A")) {
+        if (userinput.equals("A") || userinput.equals("a")) {
 
             //MET constant value for cycling
             int[] array = {6, 8, 10}; //Order respective to the options below
 
+            //Number of hours of exercise will depend on the speed of the exercise due to MET value
             System.out.println("You have chosen to do cycling");
             System.out.println("""
                     Select the speed of cycling:
@@ -227,11 +282,12 @@ public class Project {
 
             }
 
-        } else if (userinput.equals("B")) {
+        } else if (userinput.equals("B") || userinput.equals("b")) {
 
             //MET constant value for running
             int[] array2 = {5, 8, 11}; //Order respective to the options below
 
+            //Number of hours of exercise will depend on the speed of the exercise due to MET value
             System.out.println("You have chosen to do running");
             System.out.println("""
                     Select the speed of cycling:
@@ -259,7 +315,7 @@ public class Project {
             }
 
         } else {
-
+            //If anything other than the inputs defined in menu is pressed, program will exit
             System.out.println("Program Exited");
         }
     }
