be39602ff5fc06c8b0eace69e6e071736eb88167
Subha <fabihafairuzz@gmail.com>
2022-03-23 23:57:59 -0600

    Writer.java done

--- src/Util/Writer.java
+++ src/Util/Writer.java
@@ -8,5 +8,32 @@
  */
 package Util;
 
+import Main.User;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class Writer {
+
+    public static void writer(String[] out_info, File fileOutput) throws IOException{
+
+        if(fileOutput.exists() && fileOutput.canWrite()){
+            try{
+                FileWriter f_writer = new FileWriter(fileOutput);
+                BufferedWriter b_writer = new BufferedWriter(f_writer);
+
+                for (int i = 0; i < out_info.length; i++){
+                    b_writer.write(i + "\n");
+                }
+                b_writer.flush();
+            } catch (IOException e) {
+                System.err.println("Cannot access the file to write it!");
+            }
+        } else {
+            System.err.println("Cannot access the file to write it!");
+            System.exit(1);
+        }
+    }
 }
