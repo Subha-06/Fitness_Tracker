9fc363643ed1f2664edc83e2c252130ec722de5e
Subha <fabihafairuzz@gmail.com>
2022-04-05 14:53:19 -0600

    User create method added

--- src/Main/java/mvh/app/MainController.java
+++ src/Main/java/mvh/app/MainController.java
@@ -15,12 +15,17 @@ import javafx.scene.control.Alert;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import mvh.user.User;
+
+import java.util.HashMap;
 
 public class MainController {
 
+    private User user;
+    public static HashMap<Integer, Object> userInfo = new HashMap<>();
 
     @FXML
-    private TextField userNumber;
+    private ChoiceBox<Integer> userNumber;
     @FXML
     private TextField userName;
     @FXML
@@ -43,9 +48,33 @@ public class MainController {
     private Label rightStatus;
 
     @FXML
+    public void initialize() {
+        userNumber.getItems().addAll(1,2,3,4,5);
+
+    }
+
+    @FXML
     private void createUser(ActionEvent event) {
 
+        try{
+            int num = Integer.parseInt(String.valueOf(userNumber.getValue()));
+            String name = userName.getText();
+            int age = Integer.parseInt(userAge.getText());
+            String gender = String.valueOf(userGender.getValue());
+            double weight = Double.parseDouble(userWeight.getText());
+            double height = Double.parseDouble(userHeight.getText());
 
+            user = new User(name, gender, age, weight, height);
+
+            userInfo.put(num, user);
+
+            leftStatus.setText("User Added!");
+            rightStatus.setText("Choose from menu");
+
+        } catch (NumberFormatException e){
+            leftStatus.setText("Invalid Input!");
+            rightStatus.setText("Enter again");
+        }
     }
 
     /**
@@ -73,11 +102,6 @@ public class MainController {
     @FXML
     void closeButton(ActionEvent event) {
         Platform.exit();
-    }
-
-    @FXML
-    public void initialize() {
-
     }
 
     @FXML
